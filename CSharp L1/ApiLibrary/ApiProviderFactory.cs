d615bbfaf3ebef848911ea0f5f339c383d5c98c5
Mikhail Kinchin <kinchin@bk.ru>
2016-11-22 01:22:10 +0300

    Commit

---
+++ CSharp L1/ApiLibrary/ApiProviderFactory.cs
@@ -0,0 +1,12 @@
+﻿namespace ApiLibrary
+{
+    using ApiLibrary.Providers;
+
+    public static class ApiProviderFactory
+    {
+        public static IProvider GetApiProvider()
+        {
+            return new PublicApiProvider();
+        }
+    }
+}
---
+++ CSharp L1/ApiLibrary/Providers/PublicApiProvider.cs
@@ -0,0 +1,23 @@
+﻿
+namespace ApiLibrary.Providers
+{
+    using System;
+
+    public class PublicApiProvider : IProvider
+    {
+        public Boolean HealthStatus()
+        {
+            throw new NotImplementedException();
+        }
+
+        public String GetResultAsString()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetResult<T>()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
