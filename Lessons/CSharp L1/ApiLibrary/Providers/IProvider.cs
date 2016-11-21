e532f175611346ad8128a3eb3c7ca91f6b1e943b
Mikhail Kinchin <kinchin@bk.ru>
2016-11-22 01:22:10 +0300

    Commit

---
+++ Lessons/CSharp L1/ApiLibrary/Providers/IProvider.cs
@@ -0,0 +1,15 @@
+﻿// <copyright file="IProvider.cs" company="Mikhail Kinchin">
+// Copyright (c) 2016 All Rights Reserved
+// </copyright>
+
+namespace ApiLibrary.Providers
+{
+    public interface IProvider
+    {
+        bool HealthStatus();
+
+        string GetResultAsString();
+
+        T GetResult<T>();
+    }
+}
