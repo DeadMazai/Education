c6816e7dae1d281f8e3b47b3ac3ca33c7f9b21fc
Mikhail Kinchin (Akvelon) <v-mikinc@microsoft.com>
2016-12-12 13:08:25 +0300

    IDisposable

---
+++ Crash-course/DisposableStuff/DisposableStuff/IConnection.cs
@@ -0,0 +1,17 @@
+﻿//  -----------------------------------------------------------------------
+//  <copyright file="IConnection.cs" company="Microsoft">
+//       Copyright (c) Microsoft Corporation.  All rights reserved.
+//  </copyright>
+//  -----------------------------------------------------------------------
+
+namespace DisposableStuff
+{
+    public interface IConnection
+    {
+        string GetMessageState(string messageId);
+
+        bool SetMessageState(string messageId, string state);
+
+        void Close();
+    }
+}
---
+++ Crash-course/DisposableStuff/DisposableStuff/MessageInfoProvider.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace DisposableStuff
+{
+    public class MessageInfoProvider
+    {
+        private IConnection connection;
+        private bool isDisposed = false;
+
+        public MessageInfoProvider(IConnection connection)
+        {
+            this.connection = connection;
+        }
+
+        public string GetMessageState(string messageId)
+        {
+            this.CheckConnection();
+
+            return this.connection.GetMessageState(messageId);
+        }
+
+        public void SetMessageState(string messageId, string messageState)
+        {
+            this.CheckConnection();
+
+            this.connection.SetMessageState(messageId, messageState);
+        }
+
+        public string GetMessageStateSafe(string messageId)
+        {
+            return isDisposed ? this.connection.GetMessageState(messageId) : string.Empty;
+        }
+
+        public void SetMessageStateSafe(string messageId, string messageState)
+        {
+            if (isDisposed)
+            {
+                this.connection.SetMessageState(messageId, messageState);
+            }
+        }
+
+        private void CheckConnection()
+        {
+            if (isDisposed)
+            {
+                throw new ObjectDisposedException("Provider's connection was disposed.");
+            }
+        }
+
+        public void Dispose()
+        {
+            if (!this.isDisposed)
+            {
+                this.connection.Close();
+            }
+        }
+
+        ~MessageInfoProvider()
+        {
+            this.Dispose();
+        }
+    }
+}
