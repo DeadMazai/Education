f50973e316255bb257c92b2fe9bb9881ec604f93
Mikhail Kinchin (Akvelon) <v-mikinc@microsoft.com>
2016-12-12 13:23:21 +0300

    Singleton

---
+++ Crash-course/SingletonGuy/SingletonGuy/Singleton.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SingletonGuy
+{
+    public class Singleton
+    {
+        private static Lazy<Singleton> instance = new Lazy<Singleton>(() => new Singleton());
+        private readonly Dictionary<string, string> configuration;
+
+        private Singleton()
+        {
+            this.configuration = new Dictionary<string, string>();
+        }
+
+        public static Singleton GetInstance
+        {
+            get { return instance.Value; }
+        }
+
+        public string GetConfigValue(string key)
+        {
+            return configuration[key];
+        }
+
+        public void SetConfigValue(string key, string value)
+        {
+            configuration[key] = value;
+        }
+    }
+}
