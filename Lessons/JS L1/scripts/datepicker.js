44a473aa16680559c0959d0b675d5b42606f6463
mkinchin\Mikhail <mikhail.kinchin@gmail.com>
2017-01-19 01:04:55 +0300

    Initial commit

---
+++ Lessons/JS L1/scripts/datepicker.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Mikhail on 1/19/2017.
+ */
+
+(function() {
+
+    this.Datepicker = function() {
+        // Create global element references
+        this.closeButton = null;
+        this.modal = null;
+        this.overlay = null;
+
+        // Define option defaults
+        var defaults = {
+            className: 'fade-and-drop',
+            closeButton: true,
+            content: "",
+            maxWidth: 600,
+            minWidth: 280,
+            overlay: true
+        }
+    }
+}());
